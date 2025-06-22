a6dd494c8556cc419de2181e4b233f448fad8dc7
Ashish Dhiwar <dhiwarashish4@gmail.com>
2025-06-22 17:41:28 +0530

    feat: basic ws connection setup

--- midnight-client/src/screens/Chat.jsx
+++ midnight-client/src/screens/Chat.jsx
@@ -4,6 +4,10 @@ import wsClient from '../utils/wsConnection';
 function Chat() {
   const [message, setMessage] = useState('');
 
+  function sendWSMessage(){
+    wsClient.sendMessage()
+  }
+
   useEffect(() => {
     // Subscribe to messages from the WebSocket
     wsClient.subscribeToMessages((msg) => {
@@ -16,6 +20,7 @@ function Chat() {
       <div className='flex flex-col justify-center align-middle mt-4'>
         <div className='text-4xl text-center'>Chat</div>
         <div className='flex justify-center'><span>{ message }</span></div>
+        <button onClick={sendWSMessage} className='border-2 border-black rounded-3xl p-2 w-50'>Send Hi</button>
       </div>
     </>
   );
--- midnight-client/src/utils/wsConnection.js
+++ midnight-client/src/utils/wsConnection.js
@@ -2,7 +2,7 @@ let socket;
 let onMessageCallback = null;
 
 function connectWebSocket(url) {
-  socket = new WebSocket(url);
+  socket = new WebSocket(url, ["jwt", 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkF4aGlzaCIsImlhdCI6MTc1MDU3NDE5MX0.uRwjecfW9g698md83jj8JVL0D3wkU1WWO-KqrmVLoI8']);
 
   socket.onopen = () => {
     console.log("✅ WebSocket connected");
@@ -24,9 +24,9 @@ function connectWebSocket(url) {
   };
 }
 
-function sendMessage(message) {
+function sendMessage() {
   if (socket && socket.readyState === WebSocket.OPEN) {
-    socket.send(message);
+    socket.send(JSON.stringify({message: "ashish"}));
   }
 }
 
